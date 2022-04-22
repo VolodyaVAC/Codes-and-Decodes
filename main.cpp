686564fcef45fe210137abadeea557f08b125ff5
VolodyaVAC <94128489+VolodyaVAC@users.noreply.github.com>
2022-04-22 18:59:47 +0700

    Доделать дешифратор матричной

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 using namespace std;
 
 void etf(string s); // функция enter to file
-void zeroAndOne(string str); // шифровка нулями и еденицами
-void shifrAtabash(string st); // шифр Атабаш
-void matrixShifr(string sp); // Матричная шифровка
+string eff(); // функция enter from file
+void codeZeroAndOne(string str); // шифровка нулями и еденицами
+void codeAtabash(string st); // шифр Атабаш (сделана)
+void matrixCode(string sp); // Матричная шифровка
+void matrixDeCode(string sf); //Матричная Дешифровка
 
 int main() {
 
@@ -21,7 +23,9 @@ int main() {
 
     //shifrAtabash(str); // использование шифра Атабаша
 
-    matrixShifr(str);
+    matrixCode(str);
+
+    eff();
 
     return 0;
 }
@@ -35,6 +39,10 @@ void etf (string s)  {
     }
     f << s << endl;
     f.close();
+}
+
+string eff(){
+
 }
 
 void zeroAndOne(string sz) {
@@ -83,10 +91,9 @@ void shifrAtabash(string st){
     etf(res);
 }
 
-void matrixShifr(string sp) {
-    string key1, key2;
+void matrixCode(string sp) {
+    string key1, key2, res;
     int n, len, k = 0;
-    char ch, chn;
 
 
     len = sp.size(); //длинна строки
@@ -94,10 +101,10 @@ void matrixShifr(string sp) {
     if ((sqrt(len) - (int)sqrt(len)) > 0.001){
         n = sqrt(len) + 1;
     } else n = sqrt(len);
-    cout << "Enter two keys with lenght " << n << " : " << endl;
-    cin >> key1 >> key2;
+    do{ // ввод ключей
+        cin >> key1 >> key2;
+    } while ((key1.size() != n) and (key2.size() != n));
     char list[n+1][n+1];
-
 
     for (int i = 1; i <= n; i++){ // заполниение матрицы строкой
         for(int j = 1; j <= n; j++){
@@ -107,11 +114,63 @@ void matrixShifr(string sp) {
     }
     for (int i = 1; i <=n; i ++) list[0][i] = key1[i-1]; // заполнение 1ой строки ключом
     for (int i = 1; i <=n; i ++) list[i][0] = key2[i-1]; // заполнение 1ого столбца ключом
-    list[0][0] = NULL;
+    for (int i = 0; i <= n; i++){ // заполнение NULL'ов пробелами
+        for (int j = 0; j <= n; j++){
+            (list[i][j] == NULL) ? list[i][j] = ' ' : list[i][j] = list[i][j];
+        }
+    }
 
-    // доделать сортировку, вывод...
+    for (int i = 0; i <= n; i++) { //пузырьковая сортировка по первой строке
+        for (int j = 0; j <= n-1; j++) {
+            if (list[0][j] > list[0][j + 1]) {
+                char b;// создали дополнительную переменную
+                b = list[0][j];
+                list[0][j] = list[0][j + 1]; // меняем местами
+                list[0][j + 1] = b; // значения элементов
+                for (int k = 0; k <= n; k++){ // меняем местами столбцы под элементами
+                    b = list[k][j];
+                    list[k][j] = list[k][j+1];
+                    list[k][j+1] = b;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i <= n; i++) { //пузырьковая сортировка по первому столбцу
+        for (int j = 0; j <= n-1; j++) {
+            if (list[j][0] > list[j+1][0]) {
+                char b;// создали дополнительную переменную
+                b = list[j][0];
+                list[j][0] = list[j][0]; // меняем местами
+                list[j][0] = b; // значения элементов
+                for (int k = 0; k <= n; k++){ // меняем местами строки рядом с элементами
+                    b = list[j][k];
+                    list[j][k] = list[j+1][k];
+                    list[j+1][k] = b;
+                }
+            }
+        }
+    }
+
+    cout << endl;
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= n; j++){
+            cout << list[i][j]<< '-';
+        }
+        cout << endl;
+    }
+
+    char t;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            t = list[i][j];
+            res.push_back(t);
+        }
+    }
+    cout << res;
+
+    etf(res);
 }
-
 
 
 
