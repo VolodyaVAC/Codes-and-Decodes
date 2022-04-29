34c0dce145263555de3b66d34dda0387f4bb86ca
VolodyaVAC <94128489+VolodyaVAC@users.noreply.github.com>
2022-04-29 13:51:46 +0700

    Дописать XOR с вектром

--- main.cpp
+++ main.cpp
@@ -2,30 +2,32 @@
 #include<string>
 #include<fstream>
 #include<cmath>
+#include<vector>
 using namespace std;
 
 void etf(string s); // функция enter to file
 string eff(); // функция enter from file
-void codeZeroAndOne(string str); // шифровка нулями и еденицами
 void codeAtabash(string st); // шифр Атабаш (сделана)
-void matrixCode(string sp); // Матричная шифровка
+void deCodeAtabash(string sh); // Дешифратор шифра Атабаша (сделано)
+void matrixCode(string sp); // Матричная шифровка (вроде сделана)
 void matrixDeCode(string sf); //Матричная Дешифровка
+void codeXOR(string sx);
 
 int main() {
 
-    string str;
+    string str, *p;
 
     getline(cin, str); //Ввод текста из консоли
 
     etf(str); // Запись строки в файл
 
-    //zeroAndOne(str); // использование шифра нулями и еденицами
+    //codeAtabash(str);
 
-    //shifrAtabash(str); // использование шифра Атабаша
+    //matrixCode(str);
 
-    matrixCode(str);
+    codeXOR(str);
 
-    eff();
+    //eff();
 
     return 0;
 }
@@ -34,7 +36,7 @@ void etf (string s)  {
     ofstream f;
     f.open("C:/Users/Mazafacker/CLionProjects/untitled1/enter_file.txt", ofstream::app);
     if (!f.is_open()) {
-        cout << "Error of opening file!!!";
+        cout << "Error of opening file in eof!!!";
         exit(1);
     }
     f << s << endl;
@@ -42,30 +44,19 @@ void etf (string s)  {
 }
 
 string eff(){
-
-}
-
-void zeroAndOne(string sz) {
-    string strc, l, tr, *e;
-    int val, r;
-
-    val = int(sz[0]);
-    for(int j = 0; j < 8; j++){
-        (val%2) ? l[j] = '1' : l[j] = '0';
+    string sy;
+    ifstream file;
+    file.open("C:/Users/Mazafacker/CLionProjects/untitled1/enter_file.txt");
+    if (!file.is_open()) {
+        cout << "Error of opening file in eff!!!";
+        exit(1);
     }
-    reverse(l.begin(), l.end()); //работа с первым симвалом
-
-    for (int i = 1; i < sz.length(); i++) { //работа с остальными символами
-        val = int(sz[i]);
-        for(int j = 0; j < 8; j++){
-            (val%2) ? l[j] = '1' : l[j] = '0';
-        }
-        reverse(l.begin(), l.end()); //получение двоичного кода символа
-
-    } // закончить
+    while (!file.eof()) { getline(file, sy); }
+    cout << sy;
+    file.close();
 }
 
-void shifrAtabash(string st){
+void codeAtabash(string st){
     string res; // результат кодировки
     char ch;
     char val, n, t;
@@ -89,18 +80,21 @@ void shifrAtabash(string st){
         }
     }
     etf(res);
+    eff();
 }
 
 void matrixCode(string sp) {
     string key1, key2, res;
     int n, len, k = 0;
 
-
     len = sp.size(); //длинна строки
 
     if ((sqrt(len) - (int)sqrt(len)) > 0.001){
         n = sqrt(len) + 1;
     } else n = sqrt(len);
+
+    cout << n << endl; // вывод длинны ключа
+
     do{ // ввод ключей
         cin >> key1 >> key2;
     } while ((key1.size() != n) and (key2.size() != n));
@@ -117,6 +111,14 @@ void matrixCode(string sp) {
     for (int i = 0; i <= n; i++){ // заполнение NULL'ов пробелами
         for (int j = 0; j <= n; j++){
             (list[i][j] == NULL) ? list[i][j] = ' ' : list[i][j] = list[i][j];
+        }
+    }
+
+    int vol;
+    for (int i = 1; i <= n; i++){
+        if ((list[0][i] >= 'A') and (list[0][i] <= 'Z')){
+            vol = list[0][i] - 'A';
+            list[0][i] = 'z' - vol;
         }
     }
 
@@ -167,11 +169,120 @@ void matrixCode(string sp) {
             res.push_back(t);
         }
     }
-    cout << res;
+    cout << res << endl;
 
+    etf(res);
+
+    matrixDeCode(res);
+}
+
+void deCodeAtabash(string sh){
+    string res; // результат кодировки
+    char ch;
+    char val, n, t;
+
+    for (int i = 0; i < sh.size(); i++) { // обработка строки посимвольно
+        ch = sh[i];
+        n = ch;
+        if ((n <= 'Z') and (n >= 'A')){
+            val = 'Z' - n; //разница между номером символа и началом алфавита
+            t = 'A' + val;
+            res.push_back(t);
+        } else{
+            if(((n <= 'z') and (n >= 'a'))){
+                val = 'z' - n; //разница между номером символа и началом алфавита
+                t = 'a' + val;
+                res.push_back(t);
+            } else {
+                char t = sh[i];
+                res.push_back(t);
+            }
+        }
+    }
     etf(res);
 }
 
+void matrixDeCode(string sf){
+    string key1, key2, res, key1r, key2r;
+
+    cin >> key1 >> key2;
+
+    key1r = key1; key2r = key2;
+
+    for (int i = 0; i < key1.size(); i++){
+        for (int j = 0; j < key1.size(); j++){
+            if (key1r[j] > key1r[j+1]){
+                swap(key1r[j], key1r[j+1]);
+            }
+        }
+    }
+
+    for (int i = 0; i < key2.size(); i++){
+        for (int j = 0; j < key2.size(); j++){
+            if (key2r[j] > key2r[j+1]){
+                swap(key2r[j], key2r[j+1]);
+            }
+        }
+    }
+
+    int n, len, k = 0;
+
+    len = sf.size(); //длинна строки
+
+    if ((sqrt(len) - (int)sqrt(len)) > 0.001){
+        n = sqrt(len) + 1;
+    } else n = sqrt(len);
+    char list[n+1][n+1];
+
+    for (int i = 1; i <= n; i++){ // заполниение матрицы строкой
+        for(int j = 1; j <= n; j++){
+            list[i][j] = sf[k];
+            k++;
+        }
+    }
+    for (int i = 1; i <=n; i ++) list[0][i] = key1r[i-1]; // заполнение 1ой строки ключом
+    for (int i = 1; i <=n; i ++) list[i][0] = key2r[i-1]; // заполнение 1ого столбца ключом
+    for (int i = 0; i <= n; i++){ // заполнение NULL'ов пробелами
+        for (int j = 0; j <= n; j++){
+            (list[i][j] == NULL) ? list[i][j] = ' ' : list[i][j] = list[i][j];
+        }
+    }
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= n; j++){
+            cout << list[i][j]<< '-';
+        }
+        cout << endl;
+    }
+} //закончить матричную декодировку и найти ошибку в кодировке
+
+void codeXOR(string sx){
+    bool s, k;
+    string key, res;
+    cout << sx.size() << endl; // ввод ключа
+    do{
+        cin >> key;
+    }while (key.size() != sx.size());
+    /*
+    for( char c : sx ) {
+        bitset<8> bs(c);\
+        cout << bs; // строка в бинарный код
+    }
+    cout << endl;
+    for( char c : key ) {
+        bitset<8> bs(c);\
+        cout << bs; // строка в бинарный код
+    }
+    */
+
+    for (int i = 0; i < sx.size(); i++){
+        char ch, c, h;
+        c = sx[i]; h = key[i];
+        ch = c ^ h;
+        res.push_back(ch);
+    }
+    cout << res;
+}
 
 
 
