44031ba2cab0f58f2ffa4b3cef1d2f485239fa30
VolodyaVAC <94128489+VolodyaVAC@users.noreply.github.com>
2022-05-01 19:35:28 +0700

    матричную проанализировать и ВНИМАНИЕ ЕСТЬ ОШИБКИ по мелочи

--- DE_CODE_XOR.h
+++ DE_CODE_XOR.h
@@ -2,36 +2,32 @@
 // Created by Mazafacker on 01.05.2022.
 //
 
-
+#ifndef UNTITLED1_DE_CODE_XOR_H
+#define UNTITLED1_DE_CODE_XOR_H
 #include "includes.h"
 
-class DE_CODE_ATABASH{
+class DE_CODE_XOR{
 public:
-    static void deCodeAtabash(){
-        string sh, res; // результат кодировки
-        char ch;
-        char val, n, t;
-        cin >> sh;
-
-        for (int i = 0; i < sh.size(); i++) { // обработка строки посимвольно
-            ch = sh[i];
-            n = ch;
-            if ((n <= 'Z') and (n >= 'A')){
-                val = 'Z' - n; //разница между номером символа и началом алфавита
-                t = 'A' + val;
-                res.push_back(t);
-            } else{
-                if(((n <= 'z') and (n >= 'a'))){
-                    val = 'z' - n; //разница между номером символа и началом алфавита
-                    t = 'a' + val;
-                    res.push_back(t);
-                } else {
-                    char t = sh[i];
-                    res.push_back(t);
-                }
-            }
+    static void deCodeXOR(){
+        vector <int> res;
+        int n, l;
+        string sg, key;
+        char ch, c, h;
+        cin >> l;
+        for (int i = 0; i < l; i++){
+            cin >> n;
+            res.push_back(n);
         }
-        etf(res);
-    }
+        do{
+            cin >> key;
+        }while (key.size() != l);
+        for (int i = 0; i < l; i++){
+            c = key[i];
+            h = (char)res[i];
+            ch = c ^ h;
+            sg.push_back(ch);
+        }
+        cout << sg;
 };
 
+#endif //UNTITLED1_DE_CODE_ATABASH_H
